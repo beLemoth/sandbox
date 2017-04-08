dc6fed78e7989fbd7e1873309410bcf28904c9de
beLemoth <ilin.job@gmail.com>
2017-04-08 15:42:03 +0300

    update header, about styles

--- gulp/paths/css.foundation.js
+++ gulp/paths/css.foundation.js
@@ -1,5 +1,6 @@
 'use strict';
 
 module.exports = [
-  './bower_components/bootstrap/dist/css/bootstrap.min.css'
+    './bower_components/bootstrap/dist/css/bootstrap.min.css',
+    './bower_components/font-awesome/css/font-awesome.css'
 ];
